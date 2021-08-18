fea199c78d9f4e01f6f3811bd9a2cbaa5dea15d5
Evgenii Tolmachev <zoomex95@gmail.com>
2021-08-18 03:48:13 +0300

    ex 3 done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Choose what you want to do
+     * input 1 for use calculator
+     * input 2 for look for a biggest word
+     */
     public static void main(String[] args) {
-        calc();
+        Scanner sc = new Scanner(System.in);
+        switch (sc.next()) {
+            case ("1"):
+                calc();
+                break;
+            case ("2"):
+                lookForBiggestWord();
+                break;
+            default:
+                System.out.println("Wrong number");
+        }
+
     }
 
     /**
@@ -30,6 +46,29 @@ public class Main {
             case ("/"):
                 System.out.printf("%.4f", numberOne / numberTwo);
                 break;
+        }
+    }
+
+    /**
+     * Input count of words and press Enter,
+     * then input words, press Enter after every one.
+     * You'll see a word with biggest length.
+     */
+    public static void lookForBiggestWord() {
+        Scanner sc = new Scanner(System.in);
+        int arraysLength = sc.nextInt();
+        int maxLengthOfWord = 0;
+        String[] arrayOfWords = new String[arraysLength];
+        for (int i = 0; i < arraysLength; i++) {
+            arrayOfWords[i] = sc.next();
+            if (arrayOfWords[i].length() > maxLengthOfWord) {
+                maxLengthOfWord = arrayOfWords[i].length();
+            }
+        }
+        for (int i = 0; i < arraysLength; i++) {
+            if (arrayOfWords[i].length() == maxLengthOfWord) {
+                System.out.println(arrayOfWords[i]);
+            }
         }
     }
 }
